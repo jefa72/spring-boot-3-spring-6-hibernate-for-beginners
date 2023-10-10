2b2b14de1fc5586ed06cf7724772fe6e03c7a146
Jorge Eliécer <jorge.eliec@gmail.com>
2023-10-10 16:47:36 -0500

    use @RequestParam

--- 06-spring-boot-spring-mvc/04-adding-data-to-the-model/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/HelloWorldController.java
+++ 06-spring-boot-spring-mvc/04-adding-data-to-the-model/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/HelloWorldController.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
@@ -35,6 +36,21 @@ public class HelloWorldController {
 
         // create the message
         String result = "Yo! " + theName;
+
+        // add message to the model
+        model.addAttribute("shout", result);
+
+        return "helloworld";
+    }
+
+    @RequestMapping("/processFormV3")
+    public String letsShoutDude(@RequestParam("studentName") String theName, Model model) {
+
+        // convert the data to all caps
+        theName = theName.toUpperCase();
+
+        // create the message
+        String result = "Hey from V3! " + theName;
 
         // add message to the model
         model.addAttribute("shout", result);
